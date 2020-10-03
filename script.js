0253602e5fc9c9f435168d247649c291ba9c8fbb
mrajmal08 <ajmalg08@gmail.com>
2020-10-03 13:33:02 +0500

    complete invoice without $_POST

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 /** ad new row */
+let node = 0;
 $('.btn-add-row').on('click', () => {
-    let $lastRow = $('.item:last');
-    let $newRow = $lastRow.clone();
-
-    $newRow.find('input').val('');
-    $newRow.find('td:last').text('$0.00');
-    $newRow.insertAfter($lastRow);
-    $newRow.find('input:first').focus();
+    let lastRow = $('.item:first');
+    let newRow = lastRow.clone();
+    if (newRow[0].lastElementChild.getElementsByTagName('button').length === 0) {
+        let deleteBtnRow = document.createElement('td');
+        let deleteBtn = document.createElement('button');
+        deleteBtn.innerText = 'delete';
+        deleteBtn.classList = 'btn btn-danger mt-2 btn-sm';
+        deleteBtn.addEventListener('click', (e) => handleRowDelete(e));
+        deleteBtnRow.appendChild(deleteBtn)
+        newRow.append(deleteBtnRow);
+    }
+    newRow.find('input').val('');
+    let insertRow = $('.item:last');
+    newRow.insertAfter(insertRow[0]);
+    newRow.find('input:first').focus();
 });
+
+/** delete button handler*/
+function handleRowDelete(evt) {
+    evt.preventDefault()
+    let parentRow = evt.target.parentElement.parentNode;
+    parentRow.parentNode.removeChild(parentRow);
+}
 
 /** select type change */
 function handleTypeChange(element) {
@@ -24,30 +40,36 @@ function handleTypeChange(element) {
 
 /** calculation total */
 function calculateTotals(inputTotal) {
-    let inputElements = $(inputTotal);
-    let lastRow = inputTotal.parentElement.parentElement.lastElementChild;
     let totalInputs = inputTotal.parentElement.parentElement.querySelectorAll('input');
     let quantity1 = totalInputs[0].value;
     let quantity2 = totalInputs[1].value;
-    let tax = totalInputs[3].value;
-    let discount = totalInputs[4].value;
     let totalPrice = totalPriceOfValues(quantity1, quantity2);
     let total = totalInputs[2].value = totalPrice;
-    let getTaxPrice = taxCalculation(total, tax);
-    let price = getTaxPrice - discount;
-    lastRow.innerHTML = price;
     calculateSubtotal();
 }
 
 /** calculate subtotal */
 function calculateSubtotal() {
-    let subTotalElements = document.getElementsByClassName('subtotal');
-
+    let subTotalElements = document.getElementsByClassName('total');
     let total = 0;
     for (let item of subTotalElements) {
-        total = total + parseFloat(item.innerHTML);
+        total = total + parseFloat(item.value);
     }
-    $('.subTotalPrice').text(total);
+    document.getElementById('subTotal').value = total;
+
+}
+
+/** functoin of subtal tax and discount */
+function subCalculateTotals(item) {
+    let lastRow = item.parentElement.parentElement.lastElementChild;
+    let subElements = item.parentElement.parentElement.querySelectorAll('input');
+    let tax = subElements[1].value;
+    let discount = subElements[2].value;
+    let subTotalAll = subElements[0].value;
+    let getAfterTaxPrice = taxCalculation(subTotalAll, tax);
+    let getFinalPrice = getAfterTaxPrice - discount;
+    document.getElementById('finalPrice').value = getFinalPrice;
+
 }
 
 /**price of two fixed or hourly quantities*/
